9696417fa15d636acd68b9b40f353376ff3dcb64
Michał [Desktop] <michal.ly@wp.pl>
2017-10-06 12:50:03 +0200

    Refactor Search component to class

--- src/features/home/search-profile.tsx
+++ src/features/home/search-profile.tsx
@@ -1,4 +1,4 @@
-import React, { StatelessComponent } from "react";
+import React, { Component, StatelessComponent } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm, InjectedFormProps, ConfigProps, FormErrors, WrappedFieldProps } from "redux-form";
 
@@ -6,72 +6,77 @@ import { redirectToProfilePage } from "@src/redux/actions";
 import { fetchPhotos } from "@src/features/photos/action-creators";
 import { RootState } from "@src/redux/state";
 
+interface FormData {
+    profileName: string;
+}
 interface InputFieldProps {
     type: string;
     // label: string;
     placeholder: string;
 }
-const InputField: StatelessComponent<WrappedFieldProps & InputFieldProps> = ({
-    type,
-    // label,
-    placeholder,
-    input,
-    meta: {
-        touched,
-        error,
-        warning,
-    },
-}) => (
-    <div>
-        {/* <label>{label}</label> */}
-        <div>
-            <input
-                {...input}
-                className="form-control"
-                placeholder={placeholder}
-                type={type}
-            />
-            {touched &&
-                ((error &&
-                    <span>{error}</span>)
-                || (warning && <span>{warning}</span>))}
-        </div>
-    </div>
-);
-const MyField = Field as new () => Field<InputFieldProps>;
+class SearchProfile extends Component<InjectedFormProps<FormData>> {
 
-export interface FormData {
-    profileName: string;
-}
-const SearchProfile: StatelessComponent<InjectedFormProps<FormData>> = ({ handleSubmit }) => (
-    <form onSubmit={handleSubmit} className="input-group">
-        <MyField
-            name="profileName"
-            component={InputField}
-            type="text"
-            placeholder="Enter Instagram profile name"
-        />
-        <span className="input-group-btn">
-            <button
-                className="btn btn-primary"
-                type="submit"
-            >
-                Search
-            </button>
-        </span>
-    </form>
-);
+    render() {
+        const { handleSubmit } = this.props;
+        const MyField = Field as new () => Field<InputFieldProps>;
 
-function mapStateToProps(state: RootState): Partial<ConfigProps<FormData>> {
-    return {
-        initialValues: {
-            profileName: state.photos.profileName || "",
+        return (
+            <form onSubmit={handleSubmit} className="input-group">
+                <MyField
+                    name="profileName"
+                    component={this.renderInput}
+                    type="text"
+                    placeholder="Enter Instagram profile name"
+                />
+                <span className="input-group-btn">
+                    <button
+                        className="btn btn-primary"
+                        type="submit"
+                    >
+                        Search
+                    </button>
+                </span>
+            </form>
+        );
+    }
+
+    private renderInput: StatelessComponent<WrappedFieldProps & InputFieldProps> = ({
+        type,
+        // label,
+        placeholder,
+        input,
+        meta: {
+            touched,
+            error,
+            warning,
         },
-    };
+    }) => (
+        <div>
+            {/* <label>{label}</label> */}
+            <div>
+                <input
+                    {...input}
+                    className="form-control"
+                    placeholder={placeholder}
+                    type={type}
+                />
+                {touched &&
+                    (
+                        (error &&
+                            <span>{error}</span>
+                        )
+                    ||
+                        (warning &&
+                            <span>{warning}</span>
+                        )
+                    )
+                }
+            </div>
+        </div>
+    )
 }
-export const SearchProfileContainer =
-connect(mapStateToProps)
-(reduxForm<FormData>({
+
+const SearchProfileForm = reduxForm<FormData>({
     form: "searchProfile",
     validate: values => {
         const errors: FormErrors<FormData> = {};
@@ -85,5 +90,14 @@ connect(mapStateToProps)
         await dispatch(fetchPhotos(profileName));
         dispatch(redirectToProfilePage(profileName));
     },
-})
-(SearchProfile));
+})(SearchProfile);
+
+function mapStateToProps(state: RootState): Partial<ConfigProps<FormData>> {
+    return {
+        initialValues: {
+            profileName: state.photos.profileName || "",
+        },
+    };
+}
+
+export const SearchProfileContainer = connect(mapStateToProps)(SearchProfileForm);
